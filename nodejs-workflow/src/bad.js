2ea4682e6fa97bea8f31d27f26fc59085d19d123
pranav-shankar-git <pranavshankarcse@gmail.com>
2025-11-30 01:33:56 +0530

    react.js workflow

---
+++ nodejs-workflow/src/bad.js
@@ -0,0 +1,9 @@
+// Intentionally bad code to demonstrate ESLint rules
+var unused = 123
+console.log('This uses var and missing semicolons')
+
+function add(a, b){
+  return a+b
+}
+
+add(1,2)
---
+++ nodejs-workflow/src/good.js
@@ -0,0 +1,6 @@
+// Fixed version for reference
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(1, 2));
